14894cafb66fa700a1682337633da2df2f06559a
jamillalobo <lobo.jamilla@gmail.com>
2024-06-09 16:45:27 -0300

    testes extras

--- src/entregasDesafio/jamilla-lobo/pacote.test.js
+++ src/entregasDesafio/jamilla-lobo/pacote.test.js
@@ -32,4 +32,62 @@ describe("Retorna qual o pacote de acordo com o número de horas", () => {
 
     expect(result).toBe("pacote_premium");
   });
+
+  test("Retorna se é um pacote básico", () => {
+    const totalHoras = 50;
+
+    const result = calcularPacote(totalHoras);
+
+    expect(result).toBe("pacote_basico");
+  });
+
+  test("Retorna se é um pacote intermediario", () => {
+    const totalHoras = 100;
+
+    const result = calcularPacote(totalHoras);
+
+    expect(result).toBe("pacote_intermediario");
+  });
+
+  test("Retorna se é um pacote premmium", () => {
+    const totalHoras = 150;
+
+    const result = calcularPacote(totalHoras);
+
+    expect(result).toBe("pacote_premium");
+  });
+
+  test("Retorna se é um pacote premmium", () => {
+    const totalHoras = 200;
+
+    const result = calcularPacote(totalHoras);
+
+    expect(result).toBe("pacote_premium");
+  });
+
+  test("Retorna se não está em nenhum pacote por ser negativo", () => {
+    const totalHoras = -5;
+
+    const result = calcularPacote(totalHoras);
+
+    expect(result).not.toBe(
+      "pacote_premium" || "pacote_basico" || "pacote_intermediario"
+    );
+  });
+
+  test("Retorna se é um pacote básico", () => {
+    const totalHoras = 0;
+
+    const result = calcularPacote(totalHoras);
+
+    expect(result).toBe("pacote_basico");
+  });
+
+  test("Retorna se não está em nenhum pacote por ser um tipo inesperado", () => {
+    const totalHoras = true;
+
+    const result = calcularPacote(totalHoras);
+
+    expect(result).not.toBe(undefined);
+  });
 });
