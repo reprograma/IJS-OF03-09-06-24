57857d5e61488957e01a0d483b52b0cae1e0d7ca
zerikazz <107893436+zerikazz@users.noreply.github.com>
2024-06-09 16:41:41 -0300

    Add files via upload
    Entregas desafio

--- pacote.teste.js
+++ pacote.teste.js
@@ -1,37 +1,57 @@
-const { calcularPacote} = require("../../dominio/calculadora/Projeto/pacote");
+const { calcularPacote } = require("../../dominio/calculadora/Projeto/pacote");
 
 describe("Retorna qual o pacote de acordo com o número de horas", () => {
+    // Testes de pacotes válidos
     test("Retorna se é um pacote básico", () => {
-        const totalHoras = 49;
+        const totalHoras = 50;
         const resultado = calcularPacote(totalHoras);
 
-        expect(resultado).toBe("pacote_basico);
+        expect(resultado).toBe("pacote_basico");
     });
 
-    test("Retorna se é um pacote intermediario", () => {
-        const totalHoras = 51;
-        const resultado = calcularPacote(totalHoras);
-
-    expect(resultado).toBe("pacote_intermediario");
-});
-
-test("Retorna se é um pacote premium", () => {
-    const totalHoras = 101;
-    const resultado = calcularPacote(totalHoras);
-
-expect(resultado).toBe("pacote_premium");
-
-test("Retorna se é um pacote basico", () => {
-    const totalHoras = 50;
-    const resultado = calcularPacote(totalHoras);
-
-    expect(resultado).toBe("pacote_premium");
-
-    test("Retorna se é um pacote premium" () => {
+    test("Retorna se é um pacote intermediário", () => {
         const totalHoras = 100;
         const resultado = calcularPacote(totalHoras);
 
-        expect(resultado).toBe("pacote_premium")
-});
+        expect(resultado).toBe("pacote_intermediario");
+    });
 
+    test("Retorna se é um pacote premium", () => {
+        const totalHoras = 200;
+        const resultado = calcularPacote(totalHoras);
+
+        expect(resultado).toBe("pacote_premium");
+    });
+
+    // Testes com valores fora do intervalo esperado
+    test("Teste mais de 200 horas", () => {
+        const totalHoras = 201;
+        const resultado = calcularPacote(totalHoras);
+        
+        expect(resultado).toBe("erro");
+    });
+
+    test("Teste negativo", () => {
+        const totalHoras = -10;
+        const resultado = calcularPacote(totalHoras);
+        expect(resultado).toBe("erro");
+    });
+
+    test("Teste null", () => {
+        const totalHoras = null;
+        const resultado = calcularPacote(totalHoras);
+        expect(resultado).toBe("erro");
+    });
+
+    test("Teste vazio", () => {
+        const totalHoras = "";
+        const resultado = calcularPacote(totalHoras);
+        expect(resultado).toBe("erro");
+    });
+
+    test("Teste com caractere não numerico", () => {
+        const totalHoras = "cem";
+        const resultado = calcularPacote(totalHoras);
+        expect(resultado).toBe("erro");
+    });
 });
