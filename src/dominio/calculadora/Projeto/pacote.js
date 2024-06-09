84e13b22638bf44125b247add448e79ff4c8d411
ariane <ariane.casas@nectarcrm.com.br>
2024-06-09 16:39:47 -0300

    Alterações após a aula

--- src/dominio/calculadora/Projeto/pacote.js
+++ src/dominio/calculadora/Projeto/pacote.js
@@ -1,7 +1,11 @@
 const { MAX_HORAS_POR_PACOTE } = require('../constantes/constantes');
 
-const calcularPacote = (totalDeHorasPorProjeto) => Object.entries(MAX_HORAS_POR_PACOTE)
-  .find(([key, value]) => value >= totalDeHorasPorProjeto
-)[0];
+const calcularPacote = (totalDeHorasPorProjeto) => {
+    if (!totalDeHorasPorProjeto || (typeof totalDeHorasPorProjeto) !== 'number' || totalDeHorasPorProjeto <= 0) {
+        return "A quantidade de horas precisa serum número maior que zero."
+    }
+    const foundProject = Object.entries(MAX_HORAS_POR_PACOTE).find(([key, value]) => value >= totalDeHorasPorProjeto);
+    return foundProject ? foundProject[0] : undefined;
+}
 
 exports.calcularPacote = calcularPacote;
--- src/entregasDesafio/ariane-casas/pacote.test.js
+++ src/entregasDesafio/ariane-casas/pacote.test.js
@@ -31,5 +31,33 @@ describe("conjunto de testes para identificar qual é o pacote baseado nas horas
         const resultado = 'pacote_intermediario';
         expect(calcularPacote(numeroDeHoras)).toBe(resultado);
     })
+    
+    test("retorna se é um pacote premium com 200 horas", () => {
+        const numeroDeHoras = 200;
+        const resultado = 'pacote_premium';
+        expect(calcularPacote(numeroDeHoras)).toBe(resultado);
+    })
+    
+    test("retorna erro se o pacote tem mais de 200 horas", () => {
+        const numeroDeHoras = 201;
+        expect(calcularPacote(numeroDeHoras)).toBeUndefined();
+    })
+
+    test("retorna erro se o pacote tem horas negativas ou igual a zero", () => {
+        const numeroDeHoras = -15;
+        const resultado = "A quantidade de horas precisa serum número maior que zero.";
+        //Não gosto dessa ideia de string travada, mas foi o mais fácil de pensar rápido. kkkk
+        expect(calcularPacote(numeroDeHoras)).toBe(resultado);
+    })
+
+    test("retorna erro se não envio o parâmetro de horas", () => {
+        const resultado = "A quantidade de horas precisa serum número maior que zero.";
+        expect(calcularPacote()).toBe(resultado);
+    })
+
+    test("retorna erro se não envio o parâmetro em forma de string", () => {
+        const resultado = "A quantidade de horas precisa serum número maior que zero.";
+        expect(calcularPacote("testeString")).toBe(resultado);
+    })
 
 });
