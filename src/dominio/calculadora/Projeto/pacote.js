d01b80be0b4954354aab83036a3795fc7dca9f45
juliathemelo <juliarexsteam@hotmail.com>
2024-06-09 16:24:06 -0300

    sugestion code function (pacote)

--- src/dominio/calculadora/Projeto/pacote.js
+++ src/dominio/calculadora/Projeto/pacote.js
@@ -4,4 +4,12 @@ const calcularPacote = (totalDeHorasPorProjeto) => Object.entries(MAX_HORAS_POR_
   .find(([key, value]) => value >= totalDeHorasPorProjeto
 )[0];
 
+// fica como sugestão, mas para não gerar um erro na hora que passa um valor superior a 200 ou negativo, sugiro essa abordagem
+// if (pacote) {
+ // return pacote[0];
+//} else {
+//  return 'Nenhum pacote disponível';
+//}
+//adicionar um if e else
+
 exports.calcularPacote = calcularPacote;
